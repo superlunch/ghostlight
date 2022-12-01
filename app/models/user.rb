dd455deff5c151da3c2031706510e4e43f668d11
Scout Cox <coxscout@gmail.com>
2022-12-01 16:07:08 -0500

    Render deployment troubleshooting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
-    has secure password
+    has_secure_password
     
     belongs_to :job
     belongs_to :production
 
     validates :email, presence: true, uniqueness: true
-    validates :password_digest, :first_name, :last_name, :phone, :dob, :emergency_name, :emergency_phone, :emergency_relationship, :job_id, presence: true
+    validates :password, :first_name, :last_name, :phone, :dob, :emergency_name, :emergency_phone, :emergency_relationship, :job_id, presence: true
     validates :is_admin, inclusion: { in: [true, false] }
 end
